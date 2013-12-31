1f87c4fd3ea495513a952786ef421c90f1fad59f
nanaya <nanaya100@gmail.com>
2013-12-31 11:09:16 +0800

    test

--- test/convnn_test.py
+++ test/convnn_test.py
@@ -16,11 +16,11 @@ training_set = Dataset(np.vstack([tr_x[:40000], trd_x])/256,
 validation_set = Dataset(tr_x[40000:]/256, tr_y[40000:])
 test_set = Dataset(te_x/256, te_y)
 
-m = momentum(0.5, 0.99, end=100)
-d = decay(0.9, 10)
+m = momentum(0.9, 0.99, end=100)
+d = decay(0.3, 20)
 convnn = LeNet(28, [5, 25], [200], 10, lamda=0.01)
 tn = mbgd(convnn)
-last = tn.train(training_set, maxiter=150, batch_size=100, eta=0.1,
+last = tn.train(training_set, maxiter=200, batch_size=100, eta=0.1,
                 validation_set=validation_set, momentum=m, adjust_eta=d)
 print('validation set error: {}, test set error: {}'.format(
     convnn.errors(validation_set), convnn.errors(test_set)))
